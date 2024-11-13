eb42559a1fd3e1ad19b6fa7506ff07e4c933573a
nguyenduy0312 <160990102+nguyenduy0312@users.noreply.github.com>
2024-11-13 22:51:18 +0700

    update
    Hoàn thiện các phương thức thêm, sửa, xóa, tìm kiếm người dùng

--- src/test/TestDocumentDAO.java
+++ src/test/TestDocumentDAO.java
@@ -1,19 +1,30 @@
 package test;
 
+import DAO.UserDAO;
 import DataBase.GoogleBooksImporter;
 import DataBase.JDBCConnection;
 import DAO.DocumentDAO;
+import model.Account;
 import model.Document;
 import model.Person;
+import model.User;
 import util.Date;
+
+
+
 
 public class TestDocumentDAO {
     public static void main(String[] args) {
-        Document document2 = new Document("Book1", "Bí kíp tán gái", "Sach tán gái", "HoangDiem", "HoangOanh", 1, 1000);
+        // Document document2 = new Document("Book1", "Bí kíp tán gái", "Sach tán gái", "HoangDiem", "HoangOanh", 1, 1000);
+
         // DocumentDAO.getInstance().addDocument(document1);                                         // Thêm sách thành công
+
         // DocumentDAO.getInstance().updateDocument(document2);                                       // Cập nhật sách thành công
+
         // Document documentSearchh = DocumentDAO.getInstance().findDocumentById("Book1");            // Tìm sach theo id thành công
+
         // Document documentSearch = DocumentDAO.getInstance().findDocumentByTitle("Bí kíp tán gái"); // Tìm sách theo tên thành công
+
         // Document documentSearch = DocumentDAO.getInstance().findDocumentByCategory("Sach tán gái"); // Tìm sách theo tiêu đề thành công
     }
 }
